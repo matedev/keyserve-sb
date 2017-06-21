522896268b9678d71cd1473d154788e5508d82c1
Mate Kis-Kiraly <kis.kiraly.mate@gmail.com>
2017-06-22 00:07:04 +0300

    Separate parts into phps

--- index.php
+++ index.php
@@ -1,48 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-  <meta charset="utf-8">
-  <meta name="description" content="Keyserve">
-  <meta name="author" content="Keyserve">
-  <title>Keyserve</title>
-
-    <!-- Bootstrap -->
-    <link href="vendors/bootstrap.min.css" rel="stylesheet">
-    <link href="res/css/style.css" rel="stylesheet" type="text/css">
-    <link href="res/css/fonts.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <nav class="navbar navbar-fixed-top" id="tukisz">
-      <div class="container">
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="navbar-header">
-          <button type="button" id="nav-icon3" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-              <span></span>
-              <span></span>
-              <span></span>
-              <span></span>
-          </button>
-          <a class="navbar-brand" href="#"><img src="res/img/logo.png" height="53px"></img></a>
-          </div>
-
-          <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav navbar-right">
-              <li class="active"><a href="#">Bevezető<span class="sr-only">(current)</span></a></li>
-              <li class=""><a href="#">Szolgáltatások</a></li>
-              <li><a href="#" class="hvr-underline-reveal">Partnerek</a></li>
-              <li><a href="#">Kapcsolat</a></li>
-          </ul>
-          </div><!-- /.navbar-collapse -->
-      </div>
-    </nav>
+<?php include 'header.php';?>
 
     <div class="content-main" id="page-index">
 
@@ -170,30 +126,4 @@ hello hello hello hello hello
         </div>
       </div>
 
-      <footer class="text-center bg-blue">
-        <div class="container">
-          <div class="row">
-              <div class="col-sm-4 col-xs-6">
-                  <img src="res/img/logo.png" class="img-responsive"/>
-              </div>
-              <div class="col-sm-4 xs-hidden copyright">
-                  <small class="text-white">2017 © Keyserve Kft.</small>
-              </div>
-              <div class="col-sm-4 col-xs-6">
-                  <img src="res/img/ca.png" class="img-responsive" />
-              </div>
-          </div>
-          <div class="xs-visible text-center">
-              <div class="copyright">
-                  <small class="text-white">2017 © Keyserve Kft.</small>
-              </div> 
-          </div>
-        </div>
-      </footer>
-
-    </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="vendors/bootstrap.min.js"></script>
-    <script src="res/js/main.js"></script>
-  </body>
-</html>
+<?php include 'footer.php';?>
