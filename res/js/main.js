a5a8f969e4362ed1e53923bd57cbe3bcd8bf5442
Mate Kis-Kiraly <kis.kiraly.mate@gmail.com>
2017-06-21 23:56:40 +0300

    Responsive adjustments

--- res/js/main.js
+++ res/js/main.js
@@ -1,9 +1,15 @@
-$(document).ready(function() {
-    $(window).resize(function() {
-        var bodyheight = $("#trianglebox1").outerHeight();
-        console.log(bodyheight);
-        $("#triangle1").outerHeight(bodyheight);
-        var triangle1Width = $("#triangle1").outerWidth();
-        $("#triangle1").css({left: triangle1Width * (-1) + 1});
-    }).resize();
+$(document).ready(function(){
+	$('#nav-icon3').click(function(){
+		$(this).toggleClass('open');
+	});
+});
+
+var $cog = $('#cog'),
+    $body = $(document.body),
+    bodyHeight = $body.height();
+
+$(window).scroll(function () {
+    $('.hexagon').css({
+        'transform': 'rotate(' + ($body.scrollTop() / bodyHeight * 360) + 'deg)'
+    });
 });
