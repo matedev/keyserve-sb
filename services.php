5111588a41014e62c44baa1142efbd8687572e25
Máté Kis-Király <Mt Kis-Kirly>
2017-07-01 13:44:09 +0300

    Services page

--- services.php
+++ services.php
@@ -1,129 +1,72 @@
 <?php include 'header.php';?>
 
-    <div class="content-main" id="page-index">
+    <div class="content-main" id="page-services">
 
       <div class="splash-block">
       </div>
 
       <div id="block1" class="block">
         <div class="container text-center">
-          <p class="text-title">Bevezető</p>
+          <p class="text-title">Szolgáltatások</p>
           <p class="text-normal col-md-10 col-md-push-1">
-            Sikertörténetként éljük meg a Keyserve Informatikai Szolgáltató és Tanácsadó Kft. eddigi működését, hiszen 
-            szolgáltatás-portfóliónk, megrendelői és partneri kapcsolataink, szervezeti fejlődésünk és növekedési pályánk pontosan az induláskor megfogalmazott elképzeléseink szerint alakul. <br/>
-            A prémium tanácsadási minőségre, a kölcsönös bizalomra és a közös gondolkodásra épülő filozófiánk helyességét, a másik oldalról az eszközalapú projektekben leszállított megoldások színvonalát és értékeit megrendelői kapcsolataink kialakulásán és fejlődésén keresztül is beigazolódni látjuk.
+            Hogyan rendelkezhet hagyományokkal, szilárd szakmai alapokkal, sőt, hírnévvel egy társaság, a Keyserve Kft.?
+            Természetesen vezető munkatársain keresztül. Neves rendszerintegrátor és tanácsadó cégek által nagyvállalati, állami ügyfeleknél levezényelt jelentős informatikai projektekben szereztük az informatikai szolgáltatások menedzselésével kapcsolatos tapasztalatainkat, valamint projektvezetési gyakorlatunkat. Különféle rendszerfelügyeleti és szolgáltatástámogató eszközök implementálása mellett – és azon túl – figyelmünk egyre inkább a kapcsolódó folyamatok fejlesztése, az emberi és kommunikáció tényezők felé fordult.  Az ember–eszköz–folyamat hármasa (a jól ismert 3P
+            odell) így mindennapi gondolkodásunk legfontosabb jellegzetességévé, holisztikus megközelítésünk és munkamódszerünk bázisává vált.
           </p>
-<!--div class="outer">
-<div id="cornered">It's possible to put text up here, too
-    but if you want it to follow the slant you have to stack
-    several of these.
-</div>
-<div id="maina">Hello hello hello hello
-hello hello hello hello hello
-</div>
-</div-->
-<!--div id="asdf">
-<svg height="500" width="250">
-  <polygon points="0,250 100,0 100,500" style="fill:lime;stroke:purple;stroke-width:0" />
-</svg>
-</div-->
         </div>
       </div>
 
       <div id="block2" class="block">
         <div class="container text-center">
-          <p class="text-title text-blue">Portfólió</p>
+          <p class="text-title">Erősségeink</p>
         </div>
-        <div class="container-fluid bg-right-blue bg-blue">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-5 hexagon-container"> 
-                  <div class="hexagon hexagon-on-left"></div>       
-              </div>
-              <div class="col-md-7 bg-blue text-white overlay-text-box corner corner-on-left-blue" id="trianglebox1">
-                 <div class="">
-                  <p class="text-subtitle">Szervezetek IT működésének felmérése</p>
-                  <p class="block-with-text block-with-text-blue">
-                    A fejlesztési elképzelések megfogalmazásának ésf kivitelezésének alapvető feltétele, hogy a hol tartunk most? kérdésre ismert és adekvát legyen a válasz. A viszonyítási alapot általában a nemzetközi szabványok és ajánlások jelentik. 
-                    Izgalmas kérdés, hogy a jelenlegi szervezet és működés mennyire felel meg a best practice alapú ajánlásokban megfogalmazott követelményeknek. Ezen a területen
-                    a Keyserve Kft. az alábbi szolgáltatásokkal áll az ügyfelek rendelkezésére.
-                  </p>
-                  <button type="button" class="btn btn-dark margint-30">Tovább</button>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="service-box">
+                <div class="circle circle-100 bg-blue"></div> 
+                <div class="service-text">
+                  <h3 class="text-title text-blue">Bizalom</h3>
+                  <p>A megrendelő szervezetének működése, a vezetők 
+döntési környezete és az előttük álló kihívások megértésén túl a személyes bizalom kialakítása jelenti a sikeres és tartós 
+együttműködés bázisát.</p>
                 </div>
-
               </div>
             </div>
+            <div class="col-md-6">
+              <div class="service-box">
+                <div class="circle circle-100 bg-blue"></div>
+                <div class="service-text">
+                  <h3 class="text-title text-blue">Prémium minőség</h3>
+                  <p>A szakértelem mellett szilárd értékrend, 
+tanácsadóink sokoldalú tapasztalata és az ügyfél felé való elkötelezettség szükséges a kiemelkedő minőségű szolgáltatás kialakításához és fenntartásához.</p>
+                </div>                
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="service-box">
+                <div class="circle circle-100 bg-blue"></div>
+                <div class="service-text">
+                  <h3 class="text-title text-blue">Best practice ajánlások</h3>
+                  <p>Figyelemmel kísérjük a szakterületünkre vonatkozó 
+ajánlások és szabványok fejlődését, de nem csupán beépítjük ezeket mindennapi tevékenységünkbe, hanem aktívan részt 
+is veszünk a sikeres módszertanok magyarországi meghonosításában és elterjesztésében. </p>
+                </div>                
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="service-box">
+                <div class="circle circle-100 bg-blue"></div>
+                <div class="service-text">
+                  <h3 class="text-title text-blue">Professzionális partnerek</h3>
+                  <p>Csak professzionális partnerekkel dolgozunk,akik a közelmúlt jelentős magyarországi informatikai projektjeiből hozták tudásukat 
+és tapasztalataikat.</p>
+                </div>                
+              </div>
+            </div>            
           </div>
         </div>
       </div>
 
-      <div id="block3" class="block">
-        <div class="container-fluid bg-left-light-gray bg-light-gray">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-7 bg-light-gray text-dark overlay-text-box corner corner-on-right-light-gray" id="trianglebox1">
-                  <div class="">
-                    <p class="text-subtitle">Szervezetek IT működésének felmérése</p>
-                    <p class="block-with-text block-with-text-light-gray">
-                      A fejlesztési elképzelések megfogalmazásának ésf kivitelezésének alapvető feltétele, hogy a hol tartunk most? kérdésre ismert és adekvát legyen a válasz. A viszonyítási alapot általában a nemzetközi szabványok és ajánlások jelentik. 
-                      Izgalmas kérdés, hogy a jelenlegi szervezet és működés mennyire felel meg a best practice alapú ajánlásokban megfogalmazott követelményeknek. Ezen a területen
-                      a Keyserve Kft. az alábbi szolgáltatásokkal áll az ügyfelek rendelkezésére.
-                    </p>
-                    <button type="button" class="btn btn-blue margint-30">Tovább</button>
-                  </div>
-                </div>
-                    <div class="hexagon-container">
-                      <div class="hexagon hexagon-on-right"></div> 
-                </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div id="block4" class="block">
-        <div class="container-fluid bg-right-light-blue bg-light-blue">
-          <div class="container">
-            <div class="row">
-                <div class="col-md-5 hexagon-container">
-                  <div class="hexagon hexagon-on-left"></div> 
-                </div>
-              <div class="col-md-7 bg-light-blue text-dark overlay-text-box corner corner-on-left-light-blue" id="trianglebox1">
-                <div class="">
-                  <p class="text-subtitle">Szervezetek IT működésének felmérése</p>
-                  <p class="block-with-text block-with-text-light-blue">
-                    A fejlesztési elképzelések megfogalmazásának ésf kivitelezésének alapvető feltétele, hogy a hol tartunk most? kérdésre ismert és adekvát legyen a válasz. A viszonyítási alapot általában a nemzetközi szabványok és ajánlások jelentik. 
-                    Izgalmas kérdés, hogy a jelenlegi szervezet és működés mennyire felel meg a best practice alapú ajánlásokban megfogalmazott követelményeknek. Ezen a területen
-                    a Keyserve Kft. az alábbi szolgáltatásokkal áll az ügyfelek rendelkezésére.
-                  </p>
-                  <button type="button" class="btn btn-dark margint-30">Tovább</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div id="block5" class="block">
-        <div class="container-fluid bg-left-dark-gray bg-dark-gray">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-7 bg-dark-gray text-white overlay-text-box corner corner-on-right-dark-gray" id="trianglebox1">
-                <div class="">
-                  <p class="text-subtitle">Szervezetek IT működésének felmérése</p>
-                  <p class="block-with-text block-with-text-dark-gray">
-                    A fejlesztési elképzelések megfogalmazásának ésf kivitelezésének alapvető feltétele, hogy a hol tartunk most? kérdésre ismert és adekvát legyen a válasz. A viszonyítási alapot általában a nemzetközi szabványok és ajánlások jelentik. 
-                    Izgalmas kérdés, hogy a jelenlegi szervezet és működés mennyire felel meg a best practice alapú ajánlásokban megfogalmazott követelményeknek. Ezen a területen
-                    a Keyserve Kft. az alábbi szolgáltatásokkal áll az ügyfelek rendelkezésére.
-                  </p>
-                  <button type="button" class="btn btn-blue margint-30">Tovább</button>
-                </div>
-              </div>
-              <div class="hexagon-container">
-                <div class="hexagon hexagon-on-right"></div> 
-              </div>
-            </div>
-          </div>    
-        </div>
-      </div>
-
+    </div>      
 <?php include 'footer.php';?>
